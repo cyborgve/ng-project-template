8ba56c2a9976a261647f76870737f7df45e938d7
Richadr Iribarren <richadriribarren@gmail.com>
2024-06-02 04:13:15 -0400

    fix: :adhesive_bandage: title updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,6 @@ registerLocaleData(esVE, 'es-VE');
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng17 template';
+  title = 'ng project template';
   version = `v${packajeJson.version}`;
 }
